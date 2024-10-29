9fab303695d7e63f3750344da078dff3e89289af
arshad012 <forarshad99@gmail.com>
2024-10-29 18:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const inputs = document.querySelectorAll('.controls input');
+
+/*const inputs = document.querySelectorAll('.controls input');
 
     function handleUpdate() {
       const suffix = this.dataset.sizing || '';
@@ -7,3 +8,44 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+*/
+const video = document.getElementById('video-player');
+const toggle = document.querySelector('.toggle');
+const volumn_btn = document.querySelector('input[name=volume]');
+const playbackRate = document.querySelector('input[name=playbackRate]');
+const rewind_btn = document.querySelector('.rewind');
+const forward_btn = document.querySelector('.forward');
+
+// console.log(volumn_btn);
+
+let isVideoPlaying = false;
+toggle.onclick = () => {
+	if(isVideoPlaying) {
+		video.pause();
+	} else {
+		video.play();
+	}
+	isVideoPlaying = !isVideoPlaying;
+	toggle.innerText = isVideoPlaying ? "❚ ❚" : "►";
+}
+
+volumn_btn.onchange = (e) => {
+	const val = e.target.value;
+	video.volume = val;
+}
+
+playbackRate.onchange = (e) => {
+	const rate = e.target.value;
+	video.playbackRate = rate;
+}
+
+rewind_btn.onclick = () => {
+	video.currentTime -= 10;
+}
+
+forward_btn.onclick = () => {
+	video.currentTime += 25;
+}
+
+
+
